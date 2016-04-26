c57c3c2d22d705117456f61eec8b070e5672b0a9
Rick Sheahan <rssheah2@illinois.edu>
2016-04-26 02:08:02 -0500

    Add command line option handler for port number

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var program = require('commander');
+
 var port = 3010
+
+// Command line option handler
+program
+  .version(0.2.0)
+  .option('p, --port <n>', 'Set server port number', parseInt)
+  .parse(process.argv);
+if (program.port) {
+  port = program.port
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 });
@@ -24,5 +37,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(port, function () {
-  console.log('Space Morse server listening on ' + port);
+  console.log('Morseroulette server listening on ' + port);
 });
